f8342f75c664e7a7820005bc0a9a14b0d77220b0
Ava <info@avacollins.dev>
2026-06-24 15:00:43 -0400

    chore: add knip config

--- apps/tarot/src/app/_layout.tsx
+++ apps/tarot/src/app/_layout.tsx
@@ -5,23 +5,23 @@ import { AuthProvider } from '@/auth/auth-context';
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AuthProvider>
-        <AnimatedSplashOverlay />
-        <Stack initialRouteName="account" screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="account" />
-          <Stack.Screen name="auth/sign-in" />
-          <Stack.Screen name="auth/sign-up" />
-          <Stack.Screen name="auth/callback" />
-          <Stack.Screen name="auth/logout" />
-          <Stack.Screen name="auth/sign-out" />
-          <Stack.Screen name="index" />
-          <Stack.Screen name="explore" />
-        </Stack>
-      </AuthProvider>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            <AuthProvider>
+                <AnimatedSplashOverlay />
+                <Stack initialRouteName="account" screenOptions={{ headerShown: false }}>
+                    <Stack.Screen name="account" />
+                    <Stack.Screen name="auth/sign-in" />
+                    <Stack.Screen name="auth/sign-up" />
+                    <Stack.Screen name="auth/callback" />
+                    <Stack.Screen name="auth/logout" />
+                    <Stack.Screen name="auth/sign-out" />
+                    <Stack.Screen name="index" />
+                    <Stack.Screen name="explore" />
+                </Stack>
+            </AuthProvider>
+        </ThemeProvider>
+    );
 }
